4025136c79214c54998418e725f29abe764dd3b2
OlegEKX <olegjarl@gmail.com>
2024-05-30 16:30:41 +0300

    Завершил логику концовки марафона, то есть теперь при прохождении всего марафона сайт не падает, а предагает вернуться на главную страницу

--- ASP_CURRENT/Controllers/MarathonController.cs
+++ ASP_CURRENT/Controllers/MarathonController.cs
@@ -21,11 +21,26 @@ namespace ASP_CURRENT.Controllers
         public IActionResult Question(int id)
         {
             var question = questionStorage.GetQuestion(id);
+            var allQuestions = questionStorage.GetAllQuestions();
+            int countOfQuestions = allQuestions.Count;
             if (question == null)
             {
                 return NotFound();
             }
-            return View(question);
+
+            // додумать логику прохождения последнего вопроса 
+            // и придумать обработку последнего вопроса (чтобы после ответа на последний вопрос появлялась кнопка какая-нибудь)
+            if (question.Id != countOfQuestions)
+            {
+                ViewBag.CountOfQuestions = countOfQuestions;
+                return View(question);
+            }
+            else
+            {
+                return RedirectToAction("EndMarathon");
+            }
+            //return View(question);
+
         }
 
         [HttpPost]
@@ -37,10 +52,33 @@ namespace ASP_CURRENT.Controllers
                 return NotFound();
             }
 
+            if (id == questionStorage.GetAllQuestions().Count - 1)
+            {
+                /*ViewBag.Question
+                ViewBag.Explanation*/
+                bool isCorrectSecond = question.CorrectAnswerIndex == selectedAnswerIndex;
+                ViewBag.IsCorrectSecond = isCorrectSecond;
+
+
+                return View("EndMarathon", question);
+            }
+
             bool isCorrect = question.CorrectAnswerIndex == selectedAnswerIndex;
             ViewBag.IsCorrect = isCorrect;
             ViewBag.Explanation = question.Explanation;
             return View("Result", question);
+        }
+
+        public IActionResult Explanation(int id)
+        {
+            var question = questionStorage.GetQuestion(id);
+            ViewBag.Explanation = question.Explanation;
+            return View("Explanation", question);
+        }
+
+        public IActionResult EndMarathon()
+        {
+            return View();
         }
     }
 }
